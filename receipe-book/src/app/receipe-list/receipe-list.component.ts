2e0cd9f80133817ec7cf49e18de13ab574d53097
okulkar2 <omkr22@gmail.com>
2017-07-09 20:44:42 -0700

    Bootstrap added

--- receipe-book/src/app/receipe-list/receipe-list.component.ts
+++ receipe-book/src/app/receipe-list/receipe-list.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core'
 
 @Component({
-    selector: 'app-receipe-list'
+    selector: 'app-receipe-list',
+    template: '<h1>Things Working</h1>'
 })
+
+export class ReceipeListComponent{
+}
 

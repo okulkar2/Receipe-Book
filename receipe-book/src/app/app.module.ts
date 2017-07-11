060232640fdac6bbff5ea6ed5ab1912b2df131c1
okulkar2 <omkr22@gmail.com>
2017-07-10 19:43:25 -0700

    Receipe Detail Component added

--- receipe-book/src/app/app.module.ts
+++ receipe-book/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component'
-import { ReceipeListComponent } from './receipe-list/receipe-list.component' 
-import { ReceipeItemComponent } from './receipe-list/receipe-item/receipe-item.component'
-import { ShoppingListComponent } from './shopping-list/shopping-list.component'
+import { HeaderComponent } from './header/header.component';
+import { ReceipeListComponent } from './receipe-list/receipe-list.component'; 
+import { ReceipeItemComponent } from './receipe-list/receipe-item/receipe-item.component';
+import { ReceipeDetailComponent } from './receipe-list/receipe-detail/receipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component'
     HeaderComponent,
     ReceipeListComponent,
     ReceipeItemComponent,
+    ReceipeDetailComponent,
     ShoppingListComponent
   ],
   imports: [
---
+++ receipe-book/src/app/receipe-list/receipe-detail/receipe-detail.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-receipe-detail',
+    templateUrl: './receipe-detail.component.html'
+})
+
+export class ReceipeDetailComponent{
+}

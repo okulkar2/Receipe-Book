b0701da9b5929261fd9db13197f83258af5c3476
okulkar2 <omkr22@gmail.com>
2017-07-14 20:33:33 -0700

    Receipe List.ts and receipe list.html Updated

--- receipe-book/src/app/receipe-list/receipe-list.component.ts
+++ receipe-book/src/app/receipe-list/receipe-list.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core'
 
+import { Receipe } from './receipe.model'
+
 @Component({
     selector: 'app-receipe-list',
     templateUrl: './receipe-list.component.html'
 })
 
 export class ReceipeListComponent{
+    receipes: Receipe[]  = [
+        new Receipe('Butter Chicken', 'Butter Chicken, synonymous with modern North Indian cuisine', 
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Butter_Chicken_0000x0000_0.jpg/1200px-Butter_Chicken_0000x0000_0.jpg')
+    ];
+
+    constructor(){
+    }
+
 }
 

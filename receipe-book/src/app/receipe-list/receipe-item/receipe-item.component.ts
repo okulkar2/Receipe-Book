9e14f5d220f4d8263f83392228e5bb368e57cd16
okulkar2 <omkr22@gmail.com>
2017-08-31 18:36:51 -0700

    receipe-list and receipe item joined by data binding

--- receipe-book/src/app/receipe-list/receipe-item/receipe-item.component.ts
+++ receipe-book/src/app/receipe-list/receipe-item/receipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { Component, Input } from '@angular/core'
+import { Receipe } from '../receipe.model'
 
 @Component({
     selector: 'app-receipe-item',
@@ -6,4 +7,5 @@ import { Component } from '@angular/core'
 })
 
 export class ReceipeItemComponent{
+    @Input()element: Receipe;
 }

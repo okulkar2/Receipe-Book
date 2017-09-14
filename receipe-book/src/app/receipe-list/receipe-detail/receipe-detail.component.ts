e77af4c86f6a2ae639a1995ff4b17fdc5484317f
okulkar2 <omkr22@gmail.com>
2017-09-13 19:43:34 -0700

    Errors removed

--- receipe-book/src/app/receipe-list/receipe-detail/receipe-detail.component.ts
+++ receipe-book/src/app/receipe-list/receipe-detail/receipe-detail.component.ts
@@ -1,15 +1,6 @@
 import { Component, Input } from '@angular/core';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { Receipe } from '../receipe.model'
-=======
 import { Receipe } from '../receipe.model';
 import { ShoppingService } from '../../shopping-list/shopping.service'
->>>>>>> 1e86c75a8b3548191ef26da1d3d1bcb5040f3b94
-=======
-import { Receipe } from '../receipe.model';
-import { ShoppingService } from '../../shopping-list/shopping.service'
->>>>>>> 1e86c75a8b3548191ef26da1d3d1bcb5040f3b94
 
 @Component({
     selector: 'app-receipe-detail',
@@ -18,11 +9,6 @@ import { ShoppingService } from '../../shopping-list/shopping.service'
 
 export class ReceipeDetailComponent{
     @Input() receipeElement: Receipe
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 1e86c75a8b3548191ef26da1d3d1bcb5040f3b94
 
     constructor(private shopping: ShoppingService){}
     
@@ -30,8 +16,4 @@ export class ReceipeDetailComponent{
         this.shopping.addIngredientList(this.receipeElement.getIngredients());
         console.log("Passed to Receipe Service");
     }
-<<<<<<< HEAD
->>>>>>> 1e86c75a8b3548191ef26da1d3d1bcb5040f3b94
-=======
->>>>>>> 1e86c75a8b3548191ef26da1d3d1bcb5040f3b94
 }
--- receipe-book/src/app/receipe-list/receipe-list.component.ts
+++ receipe-book/src/app/receipe-list/receipe-list.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core'
-
 import { Receipe } from './receipe.model'
 import { ReceipeService } from './receipe.service'
 
@@ -9,31 +8,6 @@ import { ReceipeService } from './receipe.service'
     providers: [ReceipeService]
 })
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-export class ReceipeListComponent{
-    receipes: Receipe[]  = [
-        new Receipe('Butter Chicken', 'Butter Chicken, synonymous with modern North Indian cuisine', 
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Butter_Chicken_0000x0000_0.jpg/1200px-Butter_Chicken_0000x0000_0.jpg'),
-        new Receipe('Paneer Tikka Masala','Paneer tikka masala is an Indian dish of marinated paneer cheese served in a spiced gravy',
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Paneer_Tikka_masala.JPG/1280px-Paneer_Tikka_masala.JPG')   
-    ];
-
-    selectedReceipe: Receipe;
-    constructor(){
-=======
-export class ReceipeListComponent implements OnInit{
-    
-    receipes: Receipe[];
-    selectedReceipe: Receipe;
-    
-    constructor(private receipeService: ReceipeService){
-        this.receipeService.receipeSelected.subscribe(
-            (receipe: Receipe) => {
-                this.selectedReceipe = receipe
-            }
-        )
-=======
 export class ReceipeListComponent implements OnInit{
     
     receipes: Receipe[];
@@ -49,12 +23,6 @@ export class ReceipeListComponent implements OnInit{
 
     ngOnInit(){
         this.receipes = this.receipeService.getReceipes();
->>>>>>> 1e86c75a8b3548191ef26da1d3d1bcb5040f3b94
-    }
-
-    ngOnInit(){
-        this.receipes = this.receipeService.getReceipes();
->>>>>>> 1e86c75a8b3548191ef26da1d3d1bcb5040f3b94
     }
 
     onReceipeSelected(data: Receipe){
